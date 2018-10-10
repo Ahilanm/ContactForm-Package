1bc9b64e4c4815ef8c729e7664e3c131f699bc0b
ahilanm <ahilanm@dckap.com>
2018-10-10 15:07:22 +0530

    Commants added

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -14,12 +14,17 @@ class ContactServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        //route load
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        //view load
         $this->loadViewsFrom(__DIR__.'/views', 'contact');
+        //migration load
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        //contact publish
         $this->mergeConfigFrom(
             __DIR__.'/config/contact.php', 'contact'
         );
+        //views publish
         $this->publishes([
             __DIR__.'/config/contact.php' => config_path('contact.php'),
             __DIR__.'/views' => resource_path('views/vendor/contact'),
